d4556b86f0092cf0e02a801cd16695f3a8a83f8a
aylenbl <aylen.pdf@gmail.com>
2026-05-06 22:08:26 -0300

    Actualizo models.py con Categoria y Producto

--- Backend/inventario/models.py
+++ Backend/inventario/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-class Producto(models.Model):
+class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, blank=True)
+    codigo = models.CharField(max_length=50, unique=True)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    id_cat = models.ForeignKey(Categoria, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nombre
