3b189fef632486d5b00fc5d0024610a42af431f7
aylenbl <aylen.pdf@gmail.com>
2026-05-06 23:48:56 -0300

    Se actualizan las clases Producto y Categoria.

--- Backend/inventario/models.py
+++ Backend/inventario/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
-
+    id_cat = models.AutoField(primary_key=True)
+    
     def __str__(self):
         return self.nombre
 
@@ -13,6 +14,39 @@ class Producto(models.Model):
     codigo = models.CharField(max_length=50, unique=True)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     id_cat = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+from django.db import models
+
+class Categoria(models.Model):
+    id_cat = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    
+    class Meta:
+        managed = False          # Evita que Django intente crear la tabla (que solo lea, ya las creamos)
+        db_table = 'CATEGORIA'   
+        
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    id_art = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, blank=True)
+    codigo = models.CharField(max_length=50, unique=True)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    id_cat = models.ForeignKey(Categoria, on_delete=models.PROTECT, db_column='id_cat') 
+    # Agregamos db_column='id_cat' para que busque la columna exacta que creaste
+    # sin db_column='id_cat', django agrega _id al final del nombre (despues no coincide con la BD)
+
+    class Meta:
+        managed = False #Le dice a Django que no intente modificar ni crear la tabla, que lea las que ya están creadas manualmente
+        db_table = 'PRODUCTO'
 
     def __str__(self):
         return self.nombre
