9db2d87c3a394a6b663ebc390ac10d8c35987af3
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-05-08 18:45:03 -0300

    feat: agregar boton de recordarme, agregar manejo de formulario con formbuilder

--- Frontend/app-stock/src/app/features/auth/login/login.component.ts
+++ Frontend/app-stock/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Texto localizable 
+  mensajeBienvenida:string = "¡Bienvenido a ToDo Stock!";
+  // URI de imagenes
+  imagenURI:string = "assets/deposito.png";
+  // LoginForms, para detectar datos cuando se clickea el boton de iniciar sesion
+  loginForm!:FormGroup;
 
+  constructor(private formBuilder:FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      nombre:["", []],
+      password:["", []],
+      recordar:["", []],
+    })
+  }
+
+  onEnviar(_:Event) {
+    console.log(this.loginForm.value)
+  }
 }
