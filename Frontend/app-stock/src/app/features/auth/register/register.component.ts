736cede7e6346e2df17f308c04ea90d2a175b011
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-05-08 22:04:19 -0300

    feat: Pagina de registro V1, estilo brutalista, muy tosco, WIP

--- Frontend/app-stock/src/app/features/auth/register/register.component.ts
+++ Frontend/app-stock/src/app/features/auth/register/register.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  creaTuCuenta:string = "¡Create tu cuenta y llenate de sorpresas!";
+  // URI de imagenes
+  imagenURI:string = "assets/deposito.png";
+  cajaURI:string = "assets/ToDoLogosf.png";
+  // Registro de formularios
+  registerForm!:FormGroup;
 
+  constructor(private formBuilder:FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+
+    })
+  }
+
+  onEnviar(event:Event) {
+
+  }
 }
