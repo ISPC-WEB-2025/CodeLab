47754a4387d1e6ad88be07706f3860397184f412
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-05-09 18:31:04 -0300

    feat: agregar servicios de autenticacion básico, de usuario temporal(se deben reemplazar estos para el proximo sprint)

---
+++ Frontend/app-stock/src/app/core/services/temp-users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+// SERVICIO TEMPORAL, REEMPLAZAR ESTO CON CONSULTAS ATRAVES DE API'S RESTFUL A UNA BDD
+export class TempUsersService {
+  private listaUsuarios:{}[] = [
+    {id : 1, nombre : "John Administrador", role: "Administrador", password: "123456789"},
+    {id : 2, nombre : "Jane Vendedora", role: "Vendedor", password: "987654321"}
+  ];
+
+  obtenerTodos() {
+    return this.listaUsuarios;
+  }
+}
---
+++ Frontend/app-stock/src/app/core/services/user-auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { TempUsersService } from './temp-users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// Este servicio es completamente inseguro, está acá unicamente para pruebas de frontend. 
+// Lo más seguro es que debas realizar la conexión a una base de datos real para sacar los datos que necesites.
+// NO UTILIZAR ESTE SERVICIO SI ESTAS DE ACUERDO CON ESTO.
+export class UserAuthService {
+  private listaUsuarios:any;
+  
+  constructor(private TempUsersService:TempUsersService) {
+    this.listaUsuarios = TempUsersService.obtenerTodos();
+  }
+
+  public autenticar(nombre:string, password:string) {
+    console.warn("Atención!: Se está utilizando un autenticador inseguro, por lo que deberás cambiarlo antes de llevar a producción este sitio web.")
+    for(const user of this.listaUsuarios) {
+      if(nombre === user.nombre && password === user.password) {
+        return user;
+      }
+    }
+    return null;
+  }
+}
