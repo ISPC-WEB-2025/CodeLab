b2b18890de423eb9729c081cf87eaa5aed315c9f
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-05-09 21:32:33 -0300

    feat: se mejora la UX de login

--- Frontend/app-stock/src/app/features/auth/login/login.component.ts
+++ Frontend/app-stock/src/app/features/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { UserAuthService } from '../../../core/services/user-auth.service';
 
@@ -14,32 +14,52 @@ import { UserAuthService } from '../../../core/services/user-auth.service';
 export class LoginComponent {
   // Texto localizable 
   // TODO(TMF): AGREGAR MAS STRINGS QUE SE PUEDAN LOCALIZAR/SEAN TRADUCIBLES
-  mensajeBienvenida:string = "¡Bienvenido a ToDo Stock!";
+  readonly mensajeBienvenida:string = "¡Bienvenido a ToDo Stock!";
+  readonly nombreInvalido:string = "Por favor ingresa tu nombre de usuario";
+  readonly passwordNoExiste:string = "Por favor ingresa tu contraseña";
+  readonly passwordInvalido:string = "La contraseña tiene que tener 8 o más caracteres";
   // URI de imagenes
-  imagenURI:string = "assets/deposito.png";
+  readonly imagenURI:string = "assets/deposito.png";
   // LoginForms, para detectar datos cuando se clickea el boton de iniciar sesion
   loginForm!:FormGroup;
 
   constructor(private formBuilder:FormBuilder) {
     this.loginForm = this.formBuilder.group({
-      nombre:["", []],
-      password:["", []],
+      nombre:["", [Validators.required], []],
+      password:["", [Validators.required, Validators.minLength(8)], []],
       recordar:["", []],
     });
+  }
+
+  get nombre() {
+    return this.loginForm.get("nombre");
+  }
+
+  get password() {
+    return this.loginForm.get("password");
   }
 
   // Inyeccion de servicio
   private auth = inject(UserAuthService);
 
-  public onEnviar(_:Event) {
-    const loginData = this.loginForm;
-    const nombre = loginData.value.nombre;
-    const password = loginData.value.password;
+  public onEnviar(event:Event) {
+    event.preventDefault(); // Previene que el navegador haga su trabajo por defecto, ahora lo manejamos desde aqui.
 
-    const usuario = this.auth.autenticar(nombre, password);
+    // Procedemos si todos los datos del formulario estan llenados y son validos antes de contactar con el backend.
+    if(this.loginForm.valid) {
+      const loginData = this.loginForm;
+      const nombre = loginData.value.nombre;
+      const password = loginData.value.password;
 
-    if(usuario){
-      alert("Fuiste logeado con exito!");
+      // Función autenticadora
+      const usuario = this.auth.autenticar(nombre, password);
+
+      if(usuario){
+        alert("Fuiste logeado con exito!");
+      }
+    }
+    else {
+      this.loginForm.markAllAsTouched();
     }
   }
 }
