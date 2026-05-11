287bb0c902bbdc7250c6ffee86cd5d4679791712
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-05-10 23:40:54 -0300

    fix: arreglar inclusion de url roto en vendedor.view

--- Backend/vendedor/urls.py
+++ Backend/vendedor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import VendedorProductoView
+from .views import ProductoVendedorView
 
 urlpatterns = [
     path('productos/', ProductoVendedorView.as_view(), name='vendedor-productos'),
