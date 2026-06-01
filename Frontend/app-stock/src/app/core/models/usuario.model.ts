0d62cf26063911469c67318f056a248964954993
Candelaria Barjollo <candebarjollo@gmail.com>
2026-06-01 13:03:16 -0300

    TK60: creación interfaz usuario y servicio http

---
+++ Frontend/app-stock/src/app/core/models/usuario.model.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+  id?: number;              // Opcional porque al crear un usuario nuevo todavía no tiene ID
+  email: string;
+  nombre: string;
+  dni: string;
+  fecha_nacimiento: string; // Angular maneja las fechas que vienen del backend como strings 'YYYY-MM-DD'
+  rol: number;              // mandamos y recibimos el ID del rol
+  password?: string;        // Opcional porque la mostramos solo al crear/editar, pero el backend no la devuelve al listar
+}
