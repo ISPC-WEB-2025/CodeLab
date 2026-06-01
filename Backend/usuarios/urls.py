7715e12d9ffe3454671879f0d3514a3c1756df89
virginia-garcia <gavirginia1@gmail.com>
2026-06-01 17:04:50 -0300

    fix: remove duplicate router registration in urls.py

--- Backend/usuarios/urls.py
+++ Backend/usuarios/urls.py
@@ -6,8 +6,6 @@ from .views import LoginUsuarioView, UserViewSet
 router = DefaultRouter()
 router.register(r'', UserViewSet, basename='usuario')
 
-router = DefaultRouter()
-router.register(r'', UsuarioViewSet, basename='usuario')
 
 
 urlpatterns = [
