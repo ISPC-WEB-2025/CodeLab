e28c87522d234204b48fcedf87e51be8877fdb49
virginia-garcia <gavirginia1@gmail.com>
2026-06-02 11:02:28 -0300

    Refactorizo el enrutamiento de URL para incluir MovimientoViewSet para la gestión del movimiento.

--- Backend/inventario/urls.py
+++ Backend/inventario/urls.py
@@ -6,14 +6,11 @@ from .views import (
     SucursalViewSet,
     ProveedorViewSet,
     StockSucursalViewSet,
+    MovimientoViewSet,
 )
 
-# El Router genera las URLs automáticamente (api/productos/, api/productos/1/, etc.)
-router = DefaultRouter()
 
-# Ejemplos de endpoints generados por el router:
-# GET api/productos/          → lista todos los productos
-# GET api/productos/{id}/     → detalle de un producto
+router = DefaultRouter()
 
 router.register(
     r"productos", ProductoViewSet, basename="producto"
@@ -21,11 +18,8 @@ router.register(
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"sucursales", SucursalViewSet)
 router.register(r"proveedores", ProveedorViewSet)
-
-# GET api/stock/              → lista todo el stock con nombre de producto y sucursal
-# GET api/stock/{id}/         → detalle de un registro de stock
-
 router.register(r"stock", StockSucursalViewSet, basename="stock")
+router.register(r"movimientos", MovimientoViewSet, basename="movimiento")
 
 urlpatterns = [
     path("", include(router.urls)),
