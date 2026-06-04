3ec7084c19a0bd248ffac86c6ad04e8d3998f008
Candelaria Barjollo <candebarjollo@gmail.com>
2026-06-03 23:34:41 -0300

    Primer commit tk60

---
+++ Frontend/app-stock/src/app/features/lista-usuarios/lista-usuarios.component.ts
@@ -0,0 +1,36 @@
+
+import { Component, OnInit, inject } from '@angular/core';
+import { Usuario } from '../../core/models/usuario.model';
+import { UsuarioService } from '../../core/services/usuario.service';
+
+@Component({
+  selector: 'app-lista-usuarios',
+  standalone: true,
+  imports: [], 
+  templateUrl: './lista-usuarios.component.html',
+  styleUrl: './lista-usuarios.component.css'
+})
+export class ListaUsuariosComponent implements OnInit {
+  
+  usuarios: Usuario[] = [];
+  cargando: boolean = true; // Arranca en true para mostrar un spinner o texto de carga
+
+  private usuarioService = inject(UsuarioService);
+
+  ngOnInit(): void {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios(): void {
+    this.usuarioService.getUsuarios().subscribe({
+      next: (data) => {
+        this.usuarios = data;
+        this.cargando = false; // Apagamos la carga cuando llegan los datos
+      },
+      error: (err) => {
+        console.error('Error al traer los usuarios de la base de datos:', err);
+        this.cargando = false;
+      }
+    });
+  }
+}
