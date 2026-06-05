48b9b482bf5ef016cafe2d93d62b582486710827
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-06-05 18:23:01 -0300

    remove: comentario de depuracion

--- Frontend/app-stock/src/app/core/guards/auth.guard.ts
+++ Frontend/app-stock/src/app/core/guards/auth.guard.ts
@@ -6,8 +6,6 @@ export const authGuard: CanActivateFn = () => {
   const authService = inject(UserAuthService);
   const router = inject(Router);
 
-  console.log("authGuard invocado!", `isLoggedIn: ${authService.isLoggedIn()}, isAdmin: ${authService.isAdmin()}`);
-
   if (authService.isLoggedIn()) {
     return true;
   }
